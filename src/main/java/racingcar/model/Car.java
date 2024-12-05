b8536983dc9f18c000951e8c6e6edc8c3fb6d36a
hyebin <shin378378@naver.com>
2024-12-06 04:37:53 +0900

    feat : 자동차 생성하는 기능 추가하기

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+public class Car {
+    private int position=0;
+    private String name = null;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+    private List<Car> racingCars = new ArrayList<>();
+
+    public Cars(String carNames) {
+        List<String> names = splitName(carNames);
+        for (String name : names) {
+            racingCars.add(new Car(name));
+        }
+    }
+
+    private List<String> splitName(String carNames){
+        List<String> names = Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .toList();
+        return names;
+    }
+}
