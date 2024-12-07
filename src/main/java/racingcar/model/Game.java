7007fb07d1b5430b2afd3bb473c0bd6ce5127f10
hyebin <shin378378@naver.com>
2024-12-08 04:07:28 +0900

    feat : 차가 움직일 수 있는 지 확인하는 기능추가

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,5 +1,20 @@
 package racingcar.model;
 
-public class Game {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class Game {
+    private static final int MOVABLE_NUMBER_STANDARD = 4;
+
+    public void playOneGround(List){
+
+    }
+
+    private boolean checkMovable() {
+        int randomNumber = createRandomNumber();
+        return randomNumber >= MOVABLE_NUMBER_STANDARD;
+    }
+
+    private int createRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
